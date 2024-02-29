788f3284c078444efeb35a4fc96cb03e18d2971a
Bruno Cyreno da Silva <114535946+BrunoCyreno@users.noreply.github.com>
2024-02-29 16:33:10 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace OneHabit
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source = OneHabit.db";
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText = @"";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+
+    }
+}
